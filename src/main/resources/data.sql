26bd4176024692c37d41cd5ca95ef9ed036af2dc
ezema <ezemartinoli01@gmail.com>
2022-07-19 00:04:21 -0300

    Primer comit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into cliente
+(dni,apellido,nombre,nacimiento)
+ values
+(4592460,'Dowd','Esther',1943),
+(49766432,'Martinoli','Carlos',1942),
+(46866432,'Martinoli','Joaquin',2005),
+(45466432,'Perez','Fernando',1974);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+drop table IF EXISTS cliente;
+create table cliente (dni int primary key,
+apellido VARCHAR(50),
+nombre VARCHAR (50),
+nacimiento int
+);
